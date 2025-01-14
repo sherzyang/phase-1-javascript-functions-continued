7f7193d36ae734016e12cf73cd6368695b7483d9
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-13 16:29:54 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(string = "roller-skate") {
+    return `This Saturday, I want to ${string}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(String="*") {
+    return function (word="special") {
+      return `You are ${String}${word}${String}!`;
+    };
+  }
+  
